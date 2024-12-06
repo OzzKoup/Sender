b6487e344b5ea3c5b2e36cb7c6e7bccf7fa692e4
Temchenko <yaroslav20202@gmail.com>
2024-12-07 00:19:59 +0200

    init

--- src/Sender.js
+++ src/Sender.js
@@ -210,7 +210,7 @@ class Bot {
                 `Sent ping - IP: ${proxyIP}, Message: ${JSON.stringify(pingMessage)}`
                     .cyan
             );
-        }, 26000);
+        }, 10000);
     }
 }
 
